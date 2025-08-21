b529eb2cda25205e0efd9c26e2b2d95b1bf7f733
Helle Engebakken <helle.engebakken@gmail.com>
2025-08-21 15:31:19 +1000

    Added the base ItemPickUpComponent.

---
+++ source/core/src/main/com/csse3200/game/components/ItemPickUpComponent.java
@@ -0,0 +1,12 @@
+package com.csse3200.game.components;
+
+public class ItemPickUpComponent extends Component {
+
+    // Create function to determine if the player is close enough to the item to pick it up
+
+    // Create a function to actually pick up
+
+    // Create a function to check if inventory is full, call a function from inventoryComponent
+
+
+}
