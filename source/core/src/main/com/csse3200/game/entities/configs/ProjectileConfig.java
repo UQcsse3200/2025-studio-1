4439e0d95ed24b4ba4aaaaa7483f42bcb137164f
Euan-Kearney <euankearney12@gmail.com>
2025-08-24 17:17:25 +1000

    Created projectile factory and bulletPistol entity. The bullet pistol currently has the attributes of an entity and the physics of an obstacle

---
+++ source/core/src/main/com/csse3200/game/entities/configs/ProjectileConfig.java
@@ -0,0 +1,6 @@
+package com.csse3200.game.entities.configs;
+
+public class ProjectileConfig {
+
+    public PistolBulletConfig pistolBullet = new PistolBulletConfig();
+}
---
+++ source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
@@ -0,0 +1,74 @@
+package com.csse3200.game.entities.factories;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.csse3200.game.ai.tasks.AITaskComponent;
+import com.csse3200.game.components.CombatStatsComponent;
+import com.csse3200.game.components.npc.GhostAnimationController;
+import com.csse3200.game.components.TouchAttackComponent;
+import com.csse3200.game.components.tasks.ChaseTask;
+import com.csse3200.game.components.tasks.WanderTask;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.*;
+import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.PhysicsUtils;
+import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.HitboxComponent;
+import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.physics.components.PhysicsMovementComponent;
+import com.csse3200.game.rendering.AnimationRenderComponent;
+import com.csse3200.game.rendering.TextureRenderComponent;
+import com.csse3200.game.services.ServiceLocator;
+
+
+/**
+ * Factory to projectile entities with predefined components.
+ *
+ * <p>Each projectile entity type should have a creation method that returns a corresponding entity.
+ * Predefined entity properties can be loaded from configs stored as json files which are defined in
+ * "ProjectileConfig".
+ *
+ * <p>If needed, this factory can be separated into more specific factories for projectiles with
+ * similar characteristics.
+ */
+
+public class ProjectileFactory {
+
+    private static final ProjectileConfig configs =
+            FileLoader.readClass(ProjectileConfig.class, "configs/projectiles.json");
+
+
+
+    public static Entity createPistolBullet() {
+        Entity pistolBullet = createBaseProjectile();
+        PistolBulletConfig config = configs.pistolBullet;
+        pistolBullet
+                .addComponent(new CombatStatsComponent(config.health, config.base_attack))
+                .addComponent(new TextureRenderComponent("images/ammo.png"));
+        pistolBullet.scaleHeight(1.5f);
+        return pistolBullet;
+    }
+
+    private static Entity createBaseProjectile() {
+
+        Entity projectile =
+                new Entity()
+                        .addComponent(new PhysicsComponent())
+                        .addComponent(new PhysicsMovementComponent())
+                        .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE))
+                        .addComponent(new HitboxComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+        projectile.getComponent(PhysicsComponent.class).setBodyType(BodyDef.BodyType.StaticBody);
+        PhysicsUtils.setScaledCollider(projectile, 0.5f, 0.2f);
+
+        return projectile;
+    }
+
+    private ProjectileFactory() {
+        throw new IllegalStateException("Instantiating static util class");
+    }
+
+}
