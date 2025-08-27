2036b90f373fb16127213cabdf56502a2b8ec8fd
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-08-27 10:04:53 +1000

    ItemComponent created

---
+++ source/core/src/main/com/csse3200/game/components/entity/item/ItemComponent.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.components.entity.item;
+
+import com.csse3200.game.components.entity.EntityComponent;
+
+public class ItemComponent{
+
+
+    public ItemComponent(){
+
+    }
+}
