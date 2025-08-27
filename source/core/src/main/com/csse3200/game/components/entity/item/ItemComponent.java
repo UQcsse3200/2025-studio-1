8fbe468c90e106dba30353d68c5774f37940a9f0
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-08-27 10:39:04 +1000

    Added property int count to ItemComponent

--- source/core/src/main/com/csse3200/game/components/entity/item/ItemComponent.java
+++ source/core/src/main/com/csse3200/game/components/entity/item/ItemComponent.java
@@ -2,10 +2,28 @@ package com.csse3200.game.components.entity.item;
 
 import com.csse3200.game.components.entity.EntityComponent;
 
-public class ItemComponent{
+public class ItemComponent  {
+
+    //to keep track of the number of the specific item
+    private int count;
 
 
     public ItemComponent(){
-
+        super();
+        this.count = 0;  //initialised at 0 for all items
     }
+
+    public int getCount(int count){
+        return this.count;
+    }
+
+    public void setCount(int count){  //to set item_count at a given value
+        this.count = count;
+    }
+
+    //public void updateCount() -- to increment/decrement count
+
 }
+
+
+
