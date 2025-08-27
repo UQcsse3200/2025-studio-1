db00e35c7eaa5b7300b5609a468ff912c4577308
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-08-27 10:56:33 +1000

    Added property description

--- source/core/src/main/com/csse3200/game/components/entity/item/ItemComponent.java
+++ source/core/src/main/com/csse3200/game/components/entity/item/ItemComponent.java
@@ -2,18 +2,20 @@ package com.csse3200.game.components.entity.item;
 
 import com.csse3200.game.components.entity.EntityComponent;
 
-public class ItemComponent  {
+public class ItemComponent extends EntityComponent {
 
     //to keep track of the number of the specific item
     private int count;
+    private String description; //describes the item
 
 
-    public ItemComponent(){
+    public ItemComponent(int count, String description) {
         super();
         this.count = 0;  //initialised at 0 for all items
+        this.description = description;
     }
 
-    public int getCount(int count){
+    public int getCount(){
         return this.count;
     }
 
@@ -22,6 +24,17 @@ public class ItemComponent  {
     }
 
     //public void updateCount() -- to increment/decrement count
+
+    public String setDescription(){
+        return ("Item : " + this.getName()
+            + "\nID : " + this.getId()
+            + "\nType : " + this.getType()
+            + "\nCount : " + this.getCount());
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
 
 }
 
