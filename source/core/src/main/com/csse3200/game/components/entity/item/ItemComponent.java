6afcb1fc9e0f80c843382c50b71f3e74eff80705
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-08-28 19:05:28 +1000

    Added javadoc to EntityComponent

--- source/core/src/main/com/csse3200/game/components/entity/item/ItemComponent.java
+++ source/core/src/main/com/csse3200/game/components/entity/item/ItemComponent.java
@@ -3,37 +3,63 @@ package com.csse3200.game.components.entity.item;
 import com.csse3200.game.components.entity.EntityComponent;
 
 public class ItemComponent extends EntityComponent {
-
-    //to keep track of the number of the specific item
     private int count;
     private String description; //describes the item
 
 
+    /**
+     *
+     * @param count is the present count for the item
+     * @param description gives the description for the item
+     */
     public ItemComponent(int count, String description) {
         super();
-        this.count = 0;  //initialised at 0 for all items
+        this.count = count;  //initialised at 0 for all items
         this.description = description;
     }
 
+    /**
+     * default constructor gets called if no value is passed
+     */
+    public ItemComponent() {}
+
+
+    /**
+     *
+     * @return the count for the item
+     */
     public int getCount(){
         return this.count;
     }
 
+    /**
+     *
+     * @param count sets the item count
+     */
     public void setCount(int count){  //to set item_count at a given value
         this.count = count;
     }
 
     //public void updateCount() -- to increment/decrement count
 
-    public String setDescription(){
+
+    /**
+     *
+     * @return the description for the
+     */
+    public void setDescription(){
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getDescription(){
         return ("Item : " + this.getName()
             + "\nID : " + this.getId()
             + "\nType : " + this.getType()
-            + "\nCount : " + this.getCount());
-    }
-
-    public String getDescription(){
-        return this.description;
+            + "\nCount : " + this.getCount());;
     }
 
 }
