e527f61d04140c1edf90ebc0d73530b45cfcedbe
Ashton Harms <ashton.harms@student.uq.edu.au>
2025-09-01 19:46:06 +1000

    added some new sprites and renamed the directory

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -76,8 +76,7 @@ public class MainGameScreen extends ScreenAdapter {
 
     logger.debug("Initialising main game screen entities");
     TerrainFactory terrainFactory = new TerrainFactory(renderer.getCamera());
-    ForestGameArea forestGameArea = new ForestGameArea(terrainFactory, renderer.getCamera());
-    forestGameArea = new ForestGameArea(terrainFactory);
+    forestGameArea = new ForestGameArea(terrainFactory, renderer.getCamera());
     forestGameArea.create();
   }
 
