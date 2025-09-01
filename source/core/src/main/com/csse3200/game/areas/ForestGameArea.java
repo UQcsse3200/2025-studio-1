9a6821fad5d5ae62ac15a38be50fe34547e62cbe
Beth McMeechan <e.mcmeechan@student.uq.edu.au>
2025-09-01 14:00:36 +1000

    Changed tree generation. Fixed jump using stamina.

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -120,7 +120,7 @@ public class ForestGameArea extends GameArea {
 
     for (int i = 0; i < 1000; i++) {
       GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
-      randomPos.y = 0;
+      randomPos.y = 2;
       Entity tree = ObstacleFactory.createTree();
       spawnEntityAt(tree, randomPos, true, false);
     }
