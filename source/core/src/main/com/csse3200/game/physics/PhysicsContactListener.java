1d7efa12e3dccf5c810a1879f1a7b65b3b95da11
zdhillon324 <z.dhillon@student.uq.edu.au>
2025-09-01 16:21:45 +1000

    This is the second commit trial 2

--- source/core/src/main/com/csse3200/game/physics/PhysicsContactListener.java
+++ source/core/src/main/com/csse3200/game/physics/PhysicsContactListener.java
@@ -42,10 +42,16 @@ public class PhysicsContactListener implements ContactListener {
 
 
   private void triggerEventOn(Fixture fixture, String evt, Fixture otherFixture) {
+    if (!(fixture.getBody().getUserData() instanceof BodyUserData) ||
+            !(otherFixture.getBody().getUserData() instanceof BodyUserData)) {
+      return; // Safety: skip if no entity data
+    }
+
     BodyUserData userData = (BodyUserData) fixture.getBody().getUserData();
-    if (userData != null && userData.entity != null) {
-      logger.debug("{} on entity {}", evt, userData.entity);
-      userData.entity.getEvents().trigger(evt, fixture, otherFixture);
+    BodyUserData otherUserData = (BodyUserData) otherFixture.getBody().getUserData();
+
+    if (userData.entity != null && otherUserData.entity != null) {
+      userData.entity.getEvents().trigger(evt, userData.entity, otherUserData.entity);
     }
   }
 }
