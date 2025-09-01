51dbfd8f5ce9dbb48fd9e7374531635ada80eb22
Ashton Harms <ashton.harms@student.uq.edu.au>
2025-09-01 17:16:09 +1000

    Spawning floor in all the rooms

--- source/core/src/main/com/csse3200/game/areas/Floor4GameArea.java
+++ source/core/src/main/com/csse3200/game/areas/Floor4GameArea.java
@@ -42,6 +42,7 @@ public class Floor4GameArea extends GameArea {
 
     spawnBordersAndReturnDoor();
     spawnPlayer();
+    spawnFloor();
 
     Entity ui = new Entity();
     ui.addComponent(new com.csse3200.game.components.gamearea.FloorLabelDisplay("Floor 4"));
