84db1e512d81384f311c0ba1d4b7132ac48a9f03
Helle Engebakken <helle.engebakken@gmail.com>
2025-09-01 10:23:37 +1000

    Commented out tester function to test ItemPickUp functionality, and uncommented the original method.

--- source/core/src/main/com/csse3200/game/entities/factories/ObstacleFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ObstacleFactory.java
@@ -17,46 +17,45 @@ import com.csse3200.game.components.entity.item.ItemComponent;
  *
  * <p>Each obstacle entity type should have a creation method that returns a corresponding entity.
  */
-// TODO uncomment when finished testing --(original obstaclefactory code )
 public class ObstacleFactory {
-//
-//  /**
-//   * Creates a tree entity.
-//   * @return entity
-//   */
-//  public static Entity createTree() {
-//    Entity tree =
-//        new Entity()
-//            .addComponent(new TextureRenderComponent("images/tree.png"))
-//            .addComponent(new PhysicsComponent())
-//            .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
-//
-//    tree.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
-//    tree.getComponent(TextureRenderComponent.class).scaleEntity();
-//    tree.scaleHeight(2.5f);
-//    PhysicsUtils.setScaledCollider(tree, 0.5f, 0.2f);
-//    return tree;
-//  }
 
-  // TODO delete this function when finished testing
+  /**
+   * Creates a tree entity.
+   * @return entity
+   */
   public static Entity createTree() {
-    Entity tree = new Entity()
-            .addComponent(new TextureRenderComponent("images/mud.png"))
+    Entity tree =
+        new Entity()
+            .addComponent(new TextureRenderComponent("images/tree.png"))
             .addComponent(new PhysicsComponent())
-            .addComponent(new ColliderComponent().setLayer(PhysicsLayer.ITEM))
-            .addComponent(new HitboxComponent())
-            .addComponent(new ItemComponent(1));
-
+            .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     tree.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     tree.getComponent(TextureRenderComponent.class).scaleEntity();
     tree.scaleHeight(2.5f);
-    PhysicsUtils.setScaledCollider(tree, 2.0f, 1.8f);
-
-    //new  tree.getComponent(PhysicsComponent.class).getBody().setUserData(tree);
-
+    PhysicsUtils.setScaledCollider(tree, 0.5f, 0.2f);
     return tree;
   }
+
+  // This function was used for testing the itemPickUp functionality, and might be useful later for further testing
+//  public static Entity createTree() {
+//    Entity tree = new Entity()
+//            .addComponent(new TextureRenderComponent("images/mud.png"))
+//            .addComponent(new PhysicsComponent())
+//            .addComponent(new ColliderComponent().setLayer(PhysicsLayer.ITEM))
+//            .addComponent(new HitboxComponent())
+//            .addComponent(new ItemComponent(1));
+//
+//
+//    tree.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+//    tree.getComponent(TextureRenderComponent.class).scaleEntity();
+//    tree.scaleHeight(2.5f);
+//    PhysicsUtils.setScaledCollider(tree, 2.0f, 1.8f);
+//
+//    //new  tree.getComponent(PhysicsComponent.class).getBody().setUserData(tree);
+//
+//    return tree;
+//  }
 
 
 
