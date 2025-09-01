a1ec236e928ecd960b6b8cee5ad67a215ea3885f
adityarajsingh <adityaraj.singh@student.uq.edu.au>
2025-09-02 02:35:09 +1000

    added some new sprites in the map and changes the ForestGameArea.java and ObstacleFactory.java

--- source/core/src/main/com/csse3200/game/entities/factories/ObstacleFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ObstacleFactory.java
@@ -12,21 +12,15 @@ import com.csse3200.game.rendering.SolidColorRenderComponent;
 
 /**
  * Factory to create obstacle entities.
- *
- * <p>Each obstacle entity type should have a creation method that returns a corresponding entity.
  */
 public class ObstacleFactory {
 
-  /**
-   * Creates a tree entity.
-   * @return entity
-   */
   public static Entity createTree() {
     Entity tree =
-        new Entity()
-            .addComponent(new TextureRenderComponent("images/tree.png"))
-            .addComponent(new PhysicsComponent())
-            .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/tree.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     tree.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     tree.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -37,10 +31,10 @@ public class ObstacleFactory {
 
   public static Entity createLongFloor() {
     Entity longFloor =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/general/LongFloor.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/general/LongFloor.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     longFloor.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     longFloor.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -51,8 +45,8 @@ public class ObstacleFactory {
 
   public static Entity createRailing() {
     Entity railing =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/general/Railing.png"));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/general/Railing.png"));
 
     railing.getComponent(TextureRenderComponent.class).scaleEntity();
     railing.scaleHeight(0.7f);
@@ -61,10 +55,10 @@ public class ObstacleFactory {
 
   public static Entity createSmallSquare() {
     Entity smallSquare =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/general/SmallSquare.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/general/SmallSquare.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     smallSquare.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     smallSquare.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -75,10 +69,10 @@ public class ObstacleFactory {
 
   public static Entity createSmallStair() {
     Entity smallStair =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/general/SmallStair.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/general/SmallStair.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     smallStair.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     smallStair.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -89,10 +83,10 @@ public class ObstacleFactory {
 
   public static Entity createSquareTile() {
     Entity squareTile =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/general/SquareTile.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/general/SquareTile.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     squareTile.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     squareTile.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -103,10 +97,10 @@ public class ObstacleFactory {
 
   public static Entity createThickFloor() {
     Entity thickFloor =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/general/ThickFloor.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/general/ThickFloor.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     thickFloor.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     thickFloor.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -117,10 +111,10 @@ public class ObstacleFactory {
 
   public static Entity createThinFloor() {
     Entity thinFloor =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/general/ThinFloor.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/general/ThinFloor.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     thinFloor.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     thinFloor.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -130,11 +124,11 @@ public class ObstacleFactory {
   }
 
   public static Entity createPurpleSpawnPad() {
-    Entity purpSpawn = 
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/spawn_pads/SpawnPadPurple.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+    Entity purpSpawn =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/spawn_pads/SpawnPadPurple.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     purpSpawn.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     purpSpawn.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -144,11 +138,11 @@ public class ObstacleFactory {
   }
 
   public static Entity createRedSpawnPad() {
-    Entity purpSpawn = 
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/spawn_pads/SpawnPadRed.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+    Entity purpSpawn =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/spawn_pads/SpawnPadRed.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     purpSpawn.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     purpSpawn.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -159,10 +153,10 @@ public class ObstacleFactory {
 
   public static Entity createCeilingLight() {
     Entity ceilingLight =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/office/CeilingLight.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/office/CeilingLight.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     ceilingLight.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     ceilingLight.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -173,10 +167,10 @@ public class ObstacleFactory {
 
   public static Entity createCrate() {
     Entity crate =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/office/Crate.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/office/Crate.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     crate.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     crate.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -187,10 +181,10 @@ public class ObstacleFactory {
 
   public static Entity createLargeShelf() {
     Entity largeShelf =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/office/LargeShelf.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/office/LargeShelf.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     largeShelf.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     largeShelf.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -201,10 +195,10 @@ public class ObstacleFactory {
 
   public static Entity createLongCeilingLight() {
     Entity longCeilingLight =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/office/LongCeilingLight.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/office/LongCeilingLight.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     longCeilingLight.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     longCeilingLight.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -215,10 +209,10 @@ public class ObstacleFactory {
 
   public static Entity createMidShelf() {
     Entity midShelf =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/office/MidShelf.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/office/MidShelf.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     midShelf.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     midShelf.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -229,10 +223,10 @@ public class ObstacleFactory {
 
   public static Entity createOfficeChair() {
     Entity officeChair =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/office/OfficeChair.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/office/OfficeChair.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     officeChair.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     officeChair.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -243,10 +237,10 @@ public class ObstacleFactory {
 
   public static Entity createOfficeDesk() {
     Entity officeDesk =
-        new Entity()
-          .addComponent(new TextureRenderComponent("foreg_sprites/office/officeDesk.png"))
-          .addComponent(new PhysicsComponent())
-          .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/office/officeDesk.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 
     officeDesk.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
     officeDesk.getComponent(TextureRenderComponent.class).scaleEntity();
@@ -255,33 +249,74 @@ public class ObstacleFactory {
     return officeDesk;
   }
 
-  /**
-   * Creates an invisible physics wall.
-   * @param width Wall width in world units
-   * @param height Wall height in world units
-   * @return Wall entity of given width and height
-   */
+  // Aditya - Security camera now collidable and bigger
+  // Aditya - Security camera as a visual-only sprite (no physics, no collision)
+  public static Entity createLargeSecurityCamera() {
+    Entity cam = new Entity()
+            .addComponent(new TextureRenderComponent("foreg_sprites/futuristic/SecurityCamera.png"));
+    cam.getComponent(TextureRenderComponent.class).scaleEntity();
+    cam.scaleHeight(2.5f); // keep your visual size
+    return cam; // no PhysicsComponent, no ColliderComponent => never collides
+  }
+
+  // Aditya - Large energy pod (2x size, collidable)
+  public static Entity createLargeEnergyPod() {
+    Entity energyPod =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/futuristic/EnergyPod.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    energyPod.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    energyPod.getComponent(TextureRenderComponent.class).scaleEntity();
+    energyPod.scaleHeight(2.0f);
+    PhysicsUtils.setScaledCollider(energyPod, 1f, 0.9f);
+    return energyPod;
+  }
+
+  // Aditya - Green storage crate (simple sprite, collidable)
+  public static Entity createStorageCrateGreen() {
+    Entity crate =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/futuristic/storage_crate_green.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    crate.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    crate.getComponent(TextureRenderComponent.class).scaleEntity();
+    crate.scaleHeight(2.0f);
+    PhysicsUtils.setScaledCollider(crate, 1f, 1f);
+    return crate;
+  }
+
+  // Aditya - Dark storage crate (simple sprite, collidable)
+  public static Entity createStorageCrateDark() {
+    Entity crate =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("foreg_sprites/futuristic/storage_crate_dark.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    crate.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    crate.getComponent(TextureRenderComponent.class).scaleEntity();
+    crate.scaleHeight(2.0f);
+    PhysicsUtils.setScaledCollider(crate, 1f, 1f);
+    return crate;
+  }
+
   public static Entity createWall(float width, float height) {
     Entity wall = new Entity()
-        .addComponent(new PhysicsComponent().setBodyType(BodyType.StaticBody))
-        .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+            .addComponent(new PhysicsComponent().setBodyType(BodyType.StaticBody))
+            .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
     wall.setScale(width, height);
     return wall;
   }
 
-  /**
-   * Creates a door trigger as a thin sensor line (no collision) that can be placed at a border.
-   * The door can be detected via collisionStart events with the player's hitbox.
-   * @param width width in world units
-   * @param height height in world units
-   * @return sensor entity
-   */
   public static Entity createDoorTrigger(float width, float height) {
     Entity trigger = new Entity()
-        .addComponent(new PhysicsComponent().setBodyType(BodyType.StaticBody))
-        .addComponent(new ColliderComponent().setLayer(PhysicsLayer.DEFAULT))
-        .addComponent(new SolidColorRenderComponent(Color.BLACK));
-    // Make collider a sensor so it doesn't block movement
+            .addComponent(new PhysicsComponent().setBodyType(BodyType.StaticBody))
+            .addComponent(new ColliderComponent().setLayer(PhysicsLayer.DEFAULT))
+            .addComponent(new SolidColorRenderComponent(Color.BLACK));
     trigger.getComponent(ColliderComponent.class).setSensor(true);
     trigger.setScale(width, height);
     return trigger;
