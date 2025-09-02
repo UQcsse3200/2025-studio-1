ba9f06f16d73e70a850043830b0a03025b272220
Ashton Harms <ashton.harms@student.uq.edu.au>
2025-09-02 22:59:13 +1000

    Fixed bugs that was causing crash, changed spawnItems function

--- source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
@@ -14,6 +14,7 @@ import com.csse3200.game.utils.math.Vector2Utils;
  */
 public class KeyboardPlayerInputComponent extends InputComponent {
   private final Vector2 walkDirection = Vector2.Zero.cpy();
+  private int focusedItem = -1;
 
   private int focusedItem = -1;
 
