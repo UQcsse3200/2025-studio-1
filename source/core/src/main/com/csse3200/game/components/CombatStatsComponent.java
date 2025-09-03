93c287aa8bb1704dcec915bb21ebace5ffdc87ba
jholmesuq <jonny.holmes@student.uq.edu.au>
2025-09-03 14:45:48 +1000

    Bug fix in apply damage

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -195,7 +195,7 @@ public class CombatStatsComponent extends Component {
    * @see #setHealth(int)
    */
   private void applyDamage(int damage) {
-    if (damage <= 0 || isDead() || !disableDamage) {
+    if (damage <= 0 || isDead() || disableDamage) {
         return;
     }
     setHealth(this.health - damage);
