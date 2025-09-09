001cf08c4cc18ef100ae5b26552bce800e99280f
xjalder <xjalder@outlook.com>
2025-09-10 02:41:27 +1000

    Created basic Consumable configs and enums.

---
+++ source/core/src/main/com/csse3200/game/entities/configs/consumables/ConsumableConfig.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.entities.configs.consumables;
+
+import com.csse3200.game.entities.configs.ItemTypes;
+
+public class ConsumableConfig {
+    public ItemTypes consumableType = ItemTypes.CONSUMABLE;
+    public boolean isProjectile;
+    public String texturePath;
+
+    public ConsumableConfig() {}
+}
