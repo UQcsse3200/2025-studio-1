f8894ece0516c4344845b09a1466cfa247093fc3
xjalder <xjalder@outlook.com>
2025-09-09 23:23:25 +1000

    Added Test file for WeaponStatsComponent

---
+++ source/core/src/main/com/csse3200/game/entities/configs/projectiles/ProjectileTarget.java
@@ -0,0 +1,9 @@
+package com.csse3200.game.entities.configs.projectiles;
+
+/**
+ * Represents the type of target a projectile can be aimed at.
+ */
+public enum ProjectileTarget {
+    PLAYER,
+    ENEMY
+}
