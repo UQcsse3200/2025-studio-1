84b9750f440adb0e8282212f37d43bb4ef69a02d
xjalder <xjalder@outlook.com>
2025-09-10 02:41:27 +1000

    Created basic Consumable configs and enums.

---
+++ source/core/src/main/com/csse3200/game/entities/configs/Consumables.java
@@ -0,0 +1,7 @@
+package com.csse3200.game.entities.configs;
+
+import com.csse3200.game.entities.configs.consumables.GenericHealItemConfig;
+
+public enum Consumables {
+    GENERIC_HEAL_ITEM(new GenericHealItemConfig())
+}
---
+++ source/core/src/main/com/csse3200/game/entities/factories/items/ConsumableFactory.java
@@ -0,0 +1,10 @@
+package com.csse3200.game.entities.factories.items;
+
+import com.csse3200.game.entities.Entity;
+
+public class ConsumableFactory {
+
+    public static Entity createConsumable() {
+
+    }
+}
