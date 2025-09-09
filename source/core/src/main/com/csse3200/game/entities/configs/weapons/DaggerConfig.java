f484c5e35a6ef33e14c4854640e9c9017a691f52
Jason Zhong <jasonyzhong06@gmail.com>
2025-09-09 18:29:13 +1000

    feat: update base weapon damage

--- source/core/src/main/com/csse3200/game/entities/configs/weapons/DaggerConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/DaggerConfig.java
@@ -2,7 +2,7 @@ package com.csse3200.game.entities.configs.weapons;
 
 public class DaggerConfig extends MeleeWeaponConfig {
     public DaggerConfig() {
-        this.baseAttack = 2;
+        this.damage = 2;
         this.hitTimer = 0.8;
         this.range = 2f;
         this.texturePath = "images/dagger.png";
--- source/core/src/main/com/csse3200/game/entities/configs/weapons/LightsaberConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/LightsaberConfig.java
@@ -5,7 +5,7 @@ package com.csse3200.game.entities.configs.weapons;
  */
 public class LightsaberConfig extends MeleeWeaponConfig {
     public LightsaberConfig() {
-        this.baseAttack = 10;
+        this.damage = 10;
         this.hitTimer = 0.8;
         this.range = 3f;
         this.texturePath = "images/lightsaberSingle.png";
