942ad1f8a37664155ef309a0fd082bdcb2db0d04
cenpnt <cenpolarbears@gmail.com>
2025-09-10 17:08:28 +1000

    Remove auto-spawn, add 'waves' terminal command to start waves in ForestGameArea

--- source/core/src/main/com/csse3200/game/ui/terminal/Terminal.java
+++ source/core/src/main/com/csse3200/game/ui/terminal/Terminal.java
@@ -4,6 +4,7 @@ import com.csse3200.game.components.Component;
 import com.csse3200.game.ui.terminal.commands.Command;
 import com.csse3200.game.ui.terminal.commands.DebugCommand;
 import com.csse3200.game.ui.terminal.commands.WinScreenCommand;
+import com.csse3200.game.ui.terminal.commands.WavesCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,6 +43,7 @@ public class Terminal extends Component {
 
     addCommand("debug", new DebugCommand());
     addCommand("winscreen", new WinScreenCommand(game));
+    addCommand("waves", new WavesCommand());
   }
 
   /** @return message entered by user */
