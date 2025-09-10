4a5e4acf04c06947bbeecb69b3f035f30aadcaf4
xjalder <xjalder@outlook.com>
2025-09-10 15:10:54 +1000

    Adjusted relevant config classes to be abstract instead and added a default case for initialising weapons.

--- source/core/src/main/com/csse3200/game/entities/configs/weapons/WeaponConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/WeaponConfig.java
@@ -2,10 +2,8 @@ package com.csse3200.game.entities.configs.weapons;
 
 import com.csse3200.game.entities.configs.ItemTypes;
 
-public class WeaponConfig {
+public abstract class WeaponConfig {
     public ItemTypes weaponType;
     public int damage;
     public String texturePath;
-
-    public WeaponConfig() {}
 }
