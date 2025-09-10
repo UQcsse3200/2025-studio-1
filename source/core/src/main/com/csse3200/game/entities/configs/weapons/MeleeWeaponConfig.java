1525218a080f819b84f46e7b0b975b426bb9968d
xjalder <xjalder@outlook.com>
2025-09-10 15:10:54 +1000

    Adjusted relevant config classes to be abstract instead and added a default case for initialising weapons.

--- source/core/src/main/com/csse3200/game/entities/configs/weapons/MeleeWeaponConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/MeleeWeaponConfig.java
@@ -2,7 +2,7 @@ package com.csse3200.game.entities.configs.weapons;
 
 import com.csse3200.game.entities.configs.ItemTypes;
 
-public class MeleeWeaponConfig extends WeaponConfig {
+public abstract class MeleeWeaponConfig extends WeaponConfig {
     public double hitTimer;
     public float range;
 
