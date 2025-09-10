ca5cfde7936726a053c554b2995fb33ddad807bf
xjalder <xjalder@outlook.com>
2025-09-10 15:38:41 +1000

    Fixed weapon config files to correctly abstract attributes from parent classes.

--- source/core/src/main/com/csse3200/game/entities/configs/weapons/PistolConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/PistolConfig.java
@@ -2,10 +2,10 @@ package com.csse3200.game.entities.configs.weapons;
 
 public class PistolConfig extends RangedWeaponConfig {
     public PistolConfig() {
-        this.ammoCapacity = 30;
-        this.reloadTimer = 1.0;
-        this.shootTimer = 0.5;
-        this.damage = 10;
-        this.texturePath = "images/pistol.png";
+        ammoCapacity = 30;
+        reloadTimer = 1.0;
+        shootTimer = 0.5;
+        damage = 10;
+        texturePath = "images/pistol.png";
     }
 }
