aa79b3052e209dece20ce953d6d86458e29f8690
Euan-KearneyUQ <euan.kearney@student.uq.edu.au>
2025-09-10 12:25:16 +1000

    Added time mocking to magazine component tests

--- source/core/src/test/com/csse3200/game/components/tasks/MagazineComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/tasks/MagazineComponentTest.java
@@ -1,23 +1,41 @@
 package com.csse3200.game.components.tasks;
 
 
+import com.badlogic.gdx.physics.box2d.Body;
 import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.components.MagazineComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.physics.PhysicsEngine;
+import com.csse3200.game.physics.PhysicsService;
+import com.csse3200.game.services.GameTime;
+import com.csse3200.game.services.ServiceLocator;
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
 class MagazineComponentTest {
 
     Entity player;
     Entity weapon;
     CombatStatsComponent combat;
     MagazineComponent magazine;
+
+    @Mock
+    PhysicsEngine engine;
+    @Mock
+    GameTime timeSource;
+
 
     @BeforeEach
     void setup() {
@@ -31,6 +49,12 @@ class MagazineComponentTest {
         player.addComponent(combat);
         weapon.addComponent(magazine);
         player.setCurrItem(weapon);
+
+        PhysicsService service = new PhysicsService(engine);
+        ServiceLocator.registerPhysicsService(service);
+        when(timeSource.getDeltaTime()).thenReturn(1f);
+        ServiceLocator.registerTimeSource(timeSource);
+
     }
 
     @Test
@@ -73,6 +97,25 @@ class MagazineComponentTest {
         Assert.assertEquals(true, magazine.reload(player));
         Assert.assertEquals(997, combat.getAmmo());
         Assert.assertEquals(12, magazine.getCurrentAmmo());
+    }
+
+    @Test
+    void updateProperlyChangesReloadingStatus() {
+
+        when(timeSource.getDeltaTime()).thenReturn(1f);
+        magazine.seTimeSinceLastReload(0f);
+        Assert.assertTrue(magazine.reloading());
+
+        magazine.update();
+        magazine.update();
+
+        Assert.assertFalse(magazine.reloading());
+    }
+
+    @AfterEach
+    void afterEach() {
+        ServiceLocator.clear();
+        reset(engine, timeSource);
     }
 
 }
