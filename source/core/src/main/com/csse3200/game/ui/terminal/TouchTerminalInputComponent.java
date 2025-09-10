c3127a0a5171a2ca0cd7a0acdb5a3adb926e7f02
Cailin Delgado <c.delgado@uqconnect.edu.au>
2025-09-10 23:05:44 +1000

    Refactored KeyboardTerminalInputComponent and TouchTerminalInputComponent, and added tests

--- source/core/src/main/com/csse3200/game/ui/terminal/TouchTerminalInputComponent.java
+++ source/core/src/main/com/csse3200/game/ui/terminal/TouchTerminalInputComponent.java
@@ -1,7 +1,6 @@
 package com.csse3200.game.ui.terminal;
 
 import com.badlogic.gdx.InputProcessor;
-import com.csse3200.game.input.InputComponent;
 
 /**
  * Input handler for the debug terminal for keyboard and touch (mouse) input.
@@ -10,35 +9,11 @@ import com.csse3200.game.input.InputComponent;
  * <p>The debug terminal can be opened and closed by scrolling vertically and a message can be entered via
  * the keyboard.
  */
-public class TouchTerminalInputComponent extends InputComponent {
-  private Terminal terminal;
-
-  public TouchTerminalInputComponent() {
-    super(10);
-  }
+public class TouchTerminalInputComponent extends BaseTerminalInputComponent {
 
   public TouchTerminalInputComponent(Terminal terminal) {
-    this();
-    this.terminal = terminal;
+      super(terminal);
   }
-
-
-  @Override
-  public void create() {
-    super.create();
-    terminal = entity.getComponent(Terminal.class);
-  }
-
-    /**
-     * Returns false because this component is not one intended to
-     * be paused
-     * @return False, as this is a component not affected by pause
-     * functionality
-     */
-    @Override
-    protected boolean isPauseable() {
-        return false;
-    }
 
   /**
    * Handles input if the terminal is open. This is because keyDown events are
@@ -50,49 +25,6 @@ public class TouchTerminalInputComponent extends InputComponent {
    */
   @Override
   public boolean keyPressed(int keycode) {
-    return terminal.isOpen();
-  }
-
-  /**
-   * Handles input if the terminal is open. If 'enter' is typed, the entered message will be
-   * processed, otherwise the message will be updated with the new character.
-   *
-   * @return whether the input was processed
-   * @see InputProcessor#keyTyped(char)
-   */
-  @Override
-  public boolean keyTyped(char character) {
-    if (!terminal.isOpen()) {
-      return false;
-    }
-
-    if (character == '\b') {
-      terminal.handleBackspace();
-      return true;
-    } else if (character == '\r' || character == '\n') {
-      if (terminal.processMessage()) {
-        terminal.toggleIsOpen();
-      }
-      terminal.setEnteredMessage("");
-      return true;
-    } else if(Character.isLetterOrDigit(character) || character == ' ') {
-      // append character to message
-      terminal.appendToMessage(character);
-      return true;
-    }
-    return false;
-  }
-
-  /**
-   * Handles input if the terminal is open. This is because keyUp events are
-   * triggered alongside keyTyped events. If the user is typing in the terminal, the input shouldn't
-   * trigger any other input handlers.
-   *
-   * @return whether the input was processed
-   * @see InputProcessor#keyUp(int)
-   */
-  @Override
-  public boolean keyReleased(int keycode) {
     return terminal.isOpen();
   }
 
