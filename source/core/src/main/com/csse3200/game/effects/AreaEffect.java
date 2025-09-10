c609502728221ac08e600dd3334064317ff025a6
xjalder <xjalder@outlook.com>
2025-09-10 17:00:45 +1000

    Fixed import statements for shop, finalised consumable initialisation and implemented ConsumableComponent

--- source/core/src/main/com/csse3200/game/effects/AreaEffect.java
+++ source/core/src/main/com/csse3200/game/effects/AreaEffect.java
@@ -1,20 +1,40 @@
 package com.csse3200.game.effects;
 
 import com.csse3200.game.entities.Entity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class AreaEffect extends Effect {
-    private static final Logger log = LoggerFactory.getLogger(AreaEffect.class);
 
     public ArrayList<Effect> effects;
     public int radius;
 
-    public AreaEffect(ArrayList<Effect> effects, int radius) {
-        this.effects = effects;
+    // Attempts to initialise an AreaEffect and checks for invalid arguments
+    public AreaEffect(List<Effect> effects, int radius) {
+        if (effects == null) {
+            throw new IllegalArgumentException("Effects list cannot be null");
+        }
+        if (effects.isEmpty()) {
+            throw new IllegalArgumentException("Effects list cannot be empty");
+        }
+        if (effects.stream().anyMatch(Objects::isNull)) {
+            throw new IllegalArgumentException("Effects list cannot contain null");
+        }
+        if (effects.stream().anyMatch(e -> e instanceof AreaEffect)) {
+            throw new IllegalArgumentException("Cannot create AreaEffect from an AreaEffect");
+        }
+        if (radius <= 0) {
+            throw new IllegalArgumentException("Radius must be positive");
+        }
+
+        this.effects = new ArrayList<>(effects);
         this.radius = radius;
+    }
+
+
+    public ArrayList<Effect> getEffects() {
+        return effects;
     }
 
     @Override
--- source/core/src/main/com/csse3200/game/entities/factories/items/ConsumableFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/items/ConsumableFactory.java
@@ -1,10 +1,29 @@
 package com.csse3200.game.entities.factories.items;
 
+import com.csse3200.game.components.ConsumableComponent;
+import com.csse3200.game.components.WeaponsStatsComponent;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.Consumables;
+import com.csse3200.game.entities.configs.consumables.ConsumableConfig;
+import com.csse3200.game.entities.configs.consumables.ProjectileConsumableConfig;
 
 public class ConsumableFactory {
 
-    public static Entity createConsumable() {
-        return new Entity();
+    public static Entity createConsumable(Consumables consumableType) {
+        ConsumableConfig config = consumableType.getConfig();
+        Entity consumable = ItemFactory.createItem(config.texturePath)
+                .addComponent(new ConsumableComponent(config.effects, config.duration));
+
+        if (config.isProjectile) {
+            setUpAsProjectileLauncher(consumable, (ProjectileConsumableConfig) config);
+        }
+
+        return consumable;
+    }
+
+    private static void setUpAsProjectileLauncher(Entity consumable, ProjectileConsumableConfig config) {
+        int damage = config.projectileDamage;
+
+        consumable.addComponent(new WeaponsStatsComponent(damage));
     }
 }
