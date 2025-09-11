df205d94bd2069951b46fbbf9253d992967cf98d
HitaarnavBhatia <s4898402@student.uq.edu.au>
2025-09-11 20:57:45 +1000

    Changed some item spawning coordinates and added javadocs

--- source/core/src/main/com/csse3200/game/entities/configs/ItemSpawnConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/ItemSpawnConfig.java
@@ -9,26 +9,35 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Sets up item spawn configurations for different maps
+ * To add items for a new map make a new method like FOREST_MAP
+ */
 public class ItemSpawnConfig {
 
+    /**
+     * Gives a list of items(including weapons) and their spawn locations for the forest map
+     * Each item can have multiple spawn locations and each location can have multiple items
+     * @return a map of item names to a list of spawn info (location and quantity)
+     */
     // Forest Map
     public static Map<String, List<ItemSpawner.ItemSpawnInfo>> FOREST_MAP() {
         Map<String, List<ItemSpawner.ItemSpawnInfo>> config = new HashMap<>();
 
         // Weapons
         config.put(Weapons.DAGGER.name(), Arrays.asList(
-                new ItemSpawner.ItemSpawnInfo(new GridPoint2(5, 5), 1),
-                new ItemSpawner.ItemSpawnInfo(new GridPoint2(10, 8), 1)
+                new ItemSpawner.ItemSpawnInfo(new GridPoint2(9, 7), 1),
+                new ItemSpawner.ItemSpawnInfo(new GridPoint2(5, 15), 1)
         ));
         config.put(Weapons.PISTOL.name(), Arrays.asList(
-                new ItemSpawner.ItemSpawnInfo(new GridPoint2(3, 7), 1),
-                new ItemSpawner.ItemSpawnInfo(new GridPoint2(12, 12), 1)
+                new ItemSpawner.ItemSpawnInfo(new GridPoint2(11, 7), 1),
+                new ItemSpawner.ItemSpawnInfo(new GridPoint2(11, 12), 1)
         ));
         config.put(Weapons.RIFLE.name(), Arrays.asList(
-                new ItemSpawner.ItemSpawnInfo(new GridPoint2(8, 2), 1)
+                new ItemSpawner.ItemSpawnInfo(new GridPoint2(16, 9), 1)
         ));
         config.put(Weapons.LIGHTSABER.name(), Arrays.asList(
-                new ItemSpawner.ItemSpawnInfo(new GridPoint2(12, 10), 1)
+                new ItemSpawner.ItemSpawnInfo(new GridPoint2(14, 7), 1)
         ));
 
 
