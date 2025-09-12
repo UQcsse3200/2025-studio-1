0a2999264593396e83215c404aaf392ad2697a16
HitaarnavBhatia <s4898402@student.uq.edu.au>
2025-09-12 14:03:46 +1000

    Added some Unit tests for item Spawner

--- source/core/src/main/com/csse3200/game/entities/spawner/ItemSpawner.java
+++ source/core/src/main/com/csse3200/game/entities/spawner/ItemSpawner.java
@@ -13,7 +13,11 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 
-
+/**
+ * Spawns items in the game area based on a provided configuration.
+ * The configuration maps item types to their spawn locations and quantities.
+ * Each item type should have a corresponding creation method in the WeaponsFactory or other relevant factory.
+ */
 public class ItemSpawner {
     private static final Logger logger = LoggerFactory.getLogger(ItemSpawner.class);
     private final GameArea gameArea;
@@ -22,6 +26,11 @@ public class ItemSpawner {
         this.gameArea = gameArea;
     }
 
+    /**
+     * Spawns items in the game area based on the provided information.
+     * Each item type is created using the appropriate factory method.
+     * @param config A map where keys are item types and values are lists of ItemSpawnInfo objects
+     */
     public void spawnItems(Map<String, List<ItemSpawnInfo>> config) {
         for (String type : config.keySet()) {
             List<ItemSpawnInfo> spawnList = config.get(type);
@@ -30,12 +39,12 @@ public class ItemSpawner {
                     Entity item = makeItem(type);
                     if (item == null)
                         continue;
-
+                    //makes the item static so its doesnt move around the map when spawned
                     PhysicsComponent physics = item.getComponent(PhysicsComponent.class);
                     if (physics != null) {
                         physics.setBodyType(BodyDef.BodyType.StaticBody);
                     }
-
+                    // spawns the item in the game area at the specified position
                     if (gameArea instanceof ForestGameArea) {
                         ((ForestGameArea) gameArea).spawnItem(item, spawninfo.position);
                     }
@@ -45,7 +54,14 @@ public class ItemSpawner {
         }
     }
 
-        private Entity makeItem(String type) {
+    /**
+     * Creates an item entity based on the provided type
+     * This method currently supports weapon types defined in the Weapons enum
+     * Additional item types like consumables, perishables, etc. can be added accordingly
+     * @param type which is the type of item to be created
+     * @return the created item entity or null if the type is unknown
+     */
+        protected Entity makeItem(String type) {
             try {
                 Weapons weapon = Weapons.valueOf(type.toUpperCase());
                 return WeaponsFactory.createWeapon(weapon);
@@ -59,7 +75,10 @@ public class ItemSpawner {
             }
         }
 
-        public static class ItemSpawnInfo {
+    /**
+     * It holds the information needed to spawn an item
+     */
+    public static class ItemSpawnInfo {
             public final GridPoint2 position;
             public final int quantity;
 
