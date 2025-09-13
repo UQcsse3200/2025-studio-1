f31e2ae71d5bf4d842066ee3546dd53f374f70e1
adityarajsingh <adityaraj.singh@student.uq.edu.au>
2025-09-13 15:02:19 +1000

    fixed test cases to do proper mocking and removed ceiling light from lobby and fixed SonarCloud issues

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -1,8 +1,6 @@
 package com.csse3200.game.areas;
 
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
@@ -10,9 +8,7 @@ import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.areas.terrain.TerrainFactory.TerrainType;
 import com.csse3200.game.components.ItemHoldComponent;
 import com.csse3200.game.components.CameraComponent;
-import com.csse3200.game.components.DoorComponent;
 import com.csse3200.game.components.KeycardGateComponent;
-import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.components.WeaponsStatsComponent;
 import com.csse3200.game.components.player.InventoryComponent;
 import com.csse3200.game.entities.Entity;
@@ -25,14 +21,11 @@ import com.csse3200.game.entities.factories.system.ObstacleFactory;
 import com.csse3200.game.entities.factories.characters.PlayerFactory;
 import com.csse3200.game.entities.factories.*;
 import com.csse3200.game.physics.components.PhysicsProjectileComponent;
-import com.csse3200.game.utils.math.GridPoint2Utils;
-import com.csse3200.game.utils.math.RandomUtils;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.components.gamearea.GameAreaDisplay;
 import com.csse3200.game.rendering.TextureRenderComponent;
-
-import javax.naming.spi.ObjectFactory;
+import java.security.SecureRandom;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -62,6 +55,7 @@ public class ForestGameArea extends GameArea {
   private static final float WALL_WIDTH = 0.1f;
 
   /** Files or pictures used by the game (enemy/props,etc.). */
+  private static final String HEART = "images/heart.png";
   private static final String[] forestTextures = {
     "images/box_boy_leaf.png",
     "images/tree.png",
@@ -102,7 +96,7 @@ public class ForestGameArea extends GameArea {
     "images/door.png",
     "images/player.png",
     "images/mud.png",
-    "images/heart.png"
+          HEART
   };
 
   /** General prop textures (floors, tiles, etc.). */
@@ -152,7 +146,6 @@ public class ForestGameArea extends GameArea {
   };
 
   private static final String[] forestTextureAtlases = {
-    "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas",
     "images/robot-2.atlas", "images/fireball.atlas", "images/blackhole.atlas", "images/Robot_1.atlas",
     "images/boss_idle.atlas",
     "images/terrain_iso_grass.atlas",
@@ -163,7 +156,6 @@ public class ForestGameArea extends GameArea {
     "images/Grokdroid.atlas",
     "images/Vroomba.atlas",
     "images/explosion_1.atlas",
-    "images/explosion_2.atlas",
     "images/explosion_2.atlas",
     "images/player.atlas",
     "images/player.atlas",
@@ -178,9 +170,9 @@ public class ForestGameArea extends GameArea {
 
   private static final String[] forestSounds = {"sounds/Impact4.ogg"};
 
-  private static final String backgroundMusic = "sounds/BGM_03_mp3.mp3";
+  private static final String BACKGROUND_MUSIC = "sounds/BGM_03.mp3";
 
-  private static final String[] forestMusic = {backgroundMusic};
+  private static final String[] forestMusic = {BACKGROUND_MUSIC};
 
 
   private final TerrainFactory terrainFactory;
@@ -249,13 +241,12 @@ public class ForestGameArea extends GameArea {
     spawnBigWall();
     // spawnGhosts();
     // spawnGhostKing();
-    int choice = (int)(Math.random() * 3);
-    if (choice == 0) {
-      spawnBoss2();
-    } else if (choice == 1) {
-      spawnRobots();
-    } else {
-      spawnBoss3();
+    SecureRandom random = new SecureRandom();
+    int choice = random.nextInt(3);
+    switch (choice) {
+      case 0 -> spawnBoss2();
+      case 1 -> spawnRobots();
+      default -> spawnBoss3();
     }
     // spawnGhostGPT();
     // spawnDeepspin();
@@ -264,7 +255,8 @@ public class ForestGameArea extends GameArea {
     playMusic();
 
     // Place a keycard on the floor so the player can unlock the door
-    float keycardX = 1f, keycardY = 15f;
+    float keycardX = 1f;
+    float keycardY = 15f;
     Entity keycard = KeycardFactory.createKeycard(1);
     keycard.setPosition(new Vector2(keycardX, keycardY));
     spawnEntity(keycard);
@@ -387,10 +379,6 @@ public class ForestGameArea extends GameArea {
       spawnEntityAt(platform, platformPos, true, false);
     }
 
-    GridPoint2 lightPos = new GridPoint2(9, 9);
-    Entity longCeilingLight = ObstacleFactory.createLongCeilingLight();
-    spawnEntityAt(longCeilingLight, lightPos, true, false);
-
     Entity officeDesk = ObstacleFactory.createOfficeDesk();
     spawnEntityAt(officeDesk, new GridPoint2(5, 11), true, false);
   }
@@ -445,9 +433,9 @@ public class ForestGameArea extends GameArea {
     GridPoint2 secondPos = new GridPoint2(10, 25);
     GridPoint2 thirdPos = new GridPoint2(15, 25);
 
-    spawnEntityAt(ItemFactory.createItem("images/heart.png"), firstPos, true, false);
-    spawnEntityAt(ItemFactory.createItem("images/heart.png"), secondPos, true, false);
-    spawnEntityAt(ItemFactory.createItem("images/heart.png"), thirdPos, true, false);
+    spawnEntityAt(ItemFactory.createItem(HEART), firstPos, true, false);
+    spawnEntityAt(ItemFactory.createItem(HEART), secondPos, true, false);
+    spawnEntityAt(ItemFactory.createItem(HEART), thirdPos, true, false);
   }
 
 
@@ -646,7 +634,7 @@ public class ForestGameArea extends GameArea {
   }
 
   private void playMusic() {
-    Music music = ServiceLocator.getResourceService().getAsset(backgroundMusic, Music.class);
+    Music music = ServiceLocator.getResourceService().getAsset(BACKGROUND_MUSIC, Music.class);
     music.setLooping(true);
     music.setVolume(0.3f);
     music.play();
@@ -693,12 +681,7 @@ public class ForestGameArea extends GameArea {
     resourceService.unloadAssets(officeTextures);
   }
 
-
-
-
   // Removed area-specific dispose to avoid double disposal during transitions
-
-
   public Entity getPlayer() {
     return player;
   }
