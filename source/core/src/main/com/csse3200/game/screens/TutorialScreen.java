fc70201be21ed231b2046d3c378402f988255470
Charmainexoxo <charmainechan@hotmail.com.hk>
2025-09-14 01:34:53 +1000

    Added logger.debug to TutorialScreen and TutorialScreenDisplay

--- source/core/src/main/com/csse3200/game/screens/TutorialScreen.java
+++ source/core/src/main/com/csse3200/game/screens/TutorialScreen.java
@@ -20,6 +20,8 @@ import com.csse3200.game.rendering.Renderer;
 import com.csse3200.game.services.GameTime;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -28,6 +30,7 @@ import java.util.List;
  * The game screen containing the tutorial screen.
  */
 public class TutorialScreen extends ScreenAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(TutorialScreen.class);
     private final GdxGame game;
     private final Renderer renderer;
     private static final String[] tutorialTextures = {
@@ -40,7 +43,7 @@ public class TutorialScreen extends ScreenAdapter {
      */
     public TutorialScreen(GdxGame game) {
         this.game = game;
-
+        logger.debug("Initialising tutorial screen service");
         // Register services
         ServiceLocator.registerInputService(new InputService());
         ServiceLocator.registerResourceService(new ResourceService());
@@ -49,7 +52,9 @@ public class TutorialScreen extends ScreenAdapter {
         ServiceLocator.registerTimeSource(new GameTime());
 
         renderer = RenderFactory.createRenderer();
+        logger.debug("Tutorial Screen renderer created");
         renderer.getCamera().getEntity().setPosition(5f, 5f);
+        logger.debug("Tutorial Screen renderer camera position set");
 
         loadAssets();
         createUI();
@@ -59,9 +64,11 @@ public class TutorialScreen extends ScreenAdapter {
      * Loads textures needed by the tutorial screen.
      */
     private void loadAssets() {
+        logger.debug("Loading tutorial screen assets");
         ResourceService resourceService = ServiceLocator.getResourceService();
         resourceService.loadTextures(tutorialTextures);
         resourceService.loadAll();
+        logger.debug("Tutorial screen assets loaded");
     }
 
     /**
@@ -69,14 +76,17 @@ public class TutorialScreen extends ScreenAdapter {
      * capturing and handling ui input.
      */
     private void createUI() {
+        logger.debug("Creating TutorialScreen UI");
         Stage stage = ServiceLocator.getRenderService().getStage();
 
         Texture bgTex = ServiceLocator.getResourceService()
                 .getAsset("images/background.png", Texture.class);
+        logger.debug("Tutorial Screen background texture asset loaded");
         Image bg = new Image(new TextureRegionDrawable(bgTex));
         bg.setFillParent(true);
         bg.setScaling(Scaling.fill);
         stage.addActor(bg);
+        logger.debug("Tutorial Screen background image added");
 
         List<TutorialStep> steps = List.of(
                 new TutorialStep("Welcome!", "Use WASD to move your character.",
@@ -86,11 +96,13 @@ public class TutorialScreen extends ScreenAdapter {
                 new TutorialStep("Pick up item", "Walk on an item to pick it up",
                         new TutorialClip("images/tutorial/move", "frame_%04d.png", 25, 12f, true))
         );
+        logger.debug("Tutorial steps created");
 
         Entity ui = new Entity();
         ui.addComponent(new TutorialScreenDisplay(game, steps))
                 .addComponent(new InputDecorator(stage, 10));
         ServiceLocator.getEntityService().register(ui);
+        logger.debug("Tutorial Screen UI created and registered");
     }
 
     /**
@@ -98,8 +110,10 @@ public class TutorialScreen extends ScreenAdapter {
      */
     @Override
     public void render(float delta) {
+        logger.debug("Render tutorial screen frame");
         ServiceLocator.getEntityService().update();
         renderer.render();
+        logger.debug("Tutorial screen frame rendered");
     }
 
     /**
@@ -107,7 +121,9 @@ public class TutorialScreen extends ScreenAdapter {
      */
     @Override
     public void resize(int width, int height) {
+        logger.debug("Resizing tutorial screen frame to {}*{}", width, height);
         renderer.resize(width, height);
+        logger.debug("Resized death screen frame to {}*{}", width, height);
     }
 
     /**
@@ -115,10 +131,13 @@ public class TutorialScreen extends ScreenAdapter {
      */
     @Override
     public void dispose() {
+        logger.debug("Disposing tutorial screen");
         renderer.dispose();
         ServiceLocator.getResourceService().unloadAssets(tutorialTextures);
         ServiceLocator.getRenderService().dispose();
         ServiceLocator.getEntityService().dispose();
+        logger.debug("Tutorial screen service disposed");
         ServiceLocator.clear();
+        logger.debug("Tutorial screen ServiceLocation cleared");
     }
 }
