3f0159470f91732fcba6ac2db04f701034bb0e4a
aam-naa <amna.arshad0205@gmail.com>
2025-09-14 20:25:29 +1000

    Refactored StationComponent and InteractableStationFactory to use configs instead of hard-coded values for better code reusability. Implements HealthStation which allows player to double maxHealth on interaction (only possible once).

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -22,6 +22,8 @@ public class CombatStatsComponent extends Component {
 
   private int thresholdForBuff = 20;
 
+  private boolean healthUpgraded;
+
 
   /**
    * Construct a combat Stats Component (Health + Attack System)
@@ -31,6 +33,7 @@ public class CombatStatsComponent extends Component {
   public CombatStatsComponent(int health) {
     setMaxHealth(health);
     setHealth(health);
+    healthUpgraded = false;
   }
 
   /**
@@ -131,5 +134,17 @@ public class CombatStatsComponent extends Component {
         return;
     }
     setHealth(this.health - damage);
+  }
+
+
+  /**
+   * <p>Doubles player maximum health when player interacts with a HealthBench. Only applied once</p>
+   */
+  public void upgradeMaxHealth() {
+    if (!healthUpgraded) {
+      healthUpgraded = true;
+      this.setMaxHealth(maxHealth * 2);
+      this.setHealth(maxHealth); // regenerate player
+    }
   }
 }
