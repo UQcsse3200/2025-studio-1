1256aab073c2202f7c3574af30cdd05831325856
HunterYCSU <hunter.yicong.su@gmail.com>
2025-09-15 17:45:00 +1000

    Addressed sonarcloud issues

--- source/core/src/main/com/csse3200/game/GdxGame.java
+++ source/core/src/main/com/csse3200/game/GdxGame.java
@@ -63,24 +63,16 @@ public class GdxGame extends Game {
    * @return new screen
    */
   private Screen newScreen(ScreenType screenType) {
-    switch (screenType) {
-      case MAIN_MENU:
-        return new MainMenuScreen(this);
-      case MAIN_GAME:
-        return new MainGameScreen(this);
-      case SETTINGS:
-        return new SettingsScreen(this);
-      case DEATH_SCREEN:
-        return new DeathScreen(this);
-      case WIN_SCREEN:
-        return new WinScreen(this);
-      case TUTORIAL_SCREEN:
-        return new TutorialScreen(this);
-      case STORY:
-        return new StoryScreen(this);
-      default:
-        return null;
-    }
+    return switch (screenType) {
+      case MAIN_MENU -> new MainMenuScreen(this);
+      case MAIN_GAME -> new MainGameScreen(this);
+      case SETTINGS -> new SettingsScreen(this);
+      case DEATH_SCREEN -> new DeathScreen(this);
+      case WIN_SCREEN -> new WinScreen(this);
+      case TUTORIAL_SCREEN -> new TutorialScreen(this);
+      case STORY -> new StoryScreen(this);
+      default -> null;
+    };
   }
 
   public enum ScreenType {
