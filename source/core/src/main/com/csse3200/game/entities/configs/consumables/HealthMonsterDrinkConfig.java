9d687e19c8829e8802d365f70c05bbf5f79ec79a
xjalder <xjalder@outlook.com>
2025-09-16 23:58:17 +1000

    Fix: renamed consumables from generic placeholders to implemented consumables such as Monster which is a Heal Item

--- source/core/src/main/com/csse3200/game/entities/configs/consumables/HealthMonsterDrinkConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/consumables/HealthMonsterDrinkConfig.java
@@ -5,9 +5,9 @@ import com.csse3200.game.effects.HealthEffect;
 import java.util.ArrayList;
 
 // Placeholder Consumable used for testing of Heal Items
-public class GenericHealItemConfig extends ConsumableConfig {
+public class HealthMonsterDrinkConfig extends ConsumableConfig {
     // Adds effect to heal 10 health
-    public GenericHealItemConfig() {
+    public HealthMonsterDrinkConfig() {
         this.effects = new ArrayList<>();
         this.effects.add(new HealthEffect(-100));
         this.texturePath = "images/monster.png";
