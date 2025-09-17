d8fdae15ab4d63ee94ed9ed8d5c203b6937b8f9f
Aaron D'Mello <aaron.r.dmello@gmail.com>
2025-09-17 10:09:42 +1000

    added tests for further error checking on SoundComponent

--- source/core/src/test/com/csse3200/game/components/SoundComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/SoundComponentTest.java
@@ -1,27 +1,51 @@
 package com.csse3200.game.components;
 
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.backends.headless.mock.audio.MockSound;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.spi.ResourceBundleControlProvider;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(GameExtension.class)
 public class SoundComponentTest {
     @Test
     void shouldNotAddSoundToNullEntity() {
         SoundComponent soundComponent = new SoundComponent();
-        assertFalse(soundComponent.registerSound("test", "sounds/enemyDeath.mp3"));
+        assertFalse(soundComponent.registerSound("test", "sound"));
     }
 
     @Test
-    void shouldAddSoundToEntity() {
+    void shouldNotRegistSoundIfNotLoaded() {
+        ResourceService resourceService = mock(ResourceService.class);
+        when(resourceService.getAsset("test", Sound.class)).thenReturn(null);
+        ServiceLocator.registerResourceService(resourceService);
+
         SoundComponent soundComponent = new SoundComponent();
         Entity entity = new Entity();
         entity.addComponent(soundComponent);
-        assertTrue(soundComponent.registerSound("test", "sounds/enemyDeath.mp3"));
+        assertFalse(soundComponent.registerSound("test", "sound"));
+    }
+
+    @Test
+    void shouldRegisterSound() {
+        ResourceService resourceService = mock(ResourceService.class);
+        MockSound mockSound = new MockSound();
+        when(resourceService.getAsset("sound", Sound.class)).thenReturn(mockSound);
+        ServiceLocator.registerResourceService(resourceService);
+
+        SoundComponent soundComponent = new SoundComponent();
+        Entity entity = new Entity();
+        entity.addComponent(soundComponent);
+        assertTrue(soundComponent.registerSound("test", "sound"));
     }
 }
