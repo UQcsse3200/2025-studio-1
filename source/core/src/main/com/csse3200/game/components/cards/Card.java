0746fab0a9c6b5313b7d88f841f87d1ba975ea96
Beth McMeechan <e.mcmeechan@student.uq.edu.au>
2025-09-23 19:40:23 +1000

    feat: added deck component containing card deck

---
+++ source/core/src/main/com/csse3200/game/components/cards/Card.java
@@ -0,0 +1,15 @@
+package com.csse3200.game.components.cards;
+
+public class Card {
+    Suit suit;
+    Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return rank.getSymbol() + suit.getSymbol();
+    }
+}
---
+++ source/core/src/main/com/csse3200/game/components/cards/DeckComponent.java
@@ -0,0 +1,35 @@
+package com.csse3200.game.components.cards;
+
+import com.csse3200.game.components.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DeckComponent extends Component {
+    List<Card> deck;
+    int position;
+
+    public DeckComponent() {
+        deck = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for(Rank rank : Rank.values()) {
+                deck.add(new Card(suit, rank));
+            }
+
+        }
+
+        Collections.shuffle(deck);
+        position = 0;
+    }
+
+    public Card drawCard(Card card) {
+        return deck.get(position);
+    }
+
+    public void resetDeck() {
+        position = 0;
+        Collections.shuffle(deck);
+    }
+
+}
