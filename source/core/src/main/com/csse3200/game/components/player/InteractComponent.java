a7a33f4371bb7dc880085a6b25d46c5895edc241
xjalder <xjalder@outlook.com>
2025-09-24 21:10:20 +1000

    Refactor: InteractComponent now uses itself as a collider to detect whether an entity is in range to be interacted with and only triggers "interact" to the closest valid entity

--- source/core/src/main/com/csse3200/game/components/player/InteractComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/InteractComponent.java
@@ -1,31 +1,68 @@
 package com.csse3200.game.components.player;
 
-import com.csse3200.game.components.Component;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.csse3200.game.entities.Entity;
-import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.physics.BodyUserData;
+import com.csse3200.game.physics.components.HitboxComponent;
+
+import java.util.ArrayList;
 
 /**
  * Handles "interact" event triggered by pressing E as seen in KeyboardPlayerInputComponent
  */
-public class InteractComponent extends Component {
+public class InteractComponent extends HitboxComponent{
+    private ArrayList<Entity> collidedEntities = new ArrayList<>();
 
     @Override
     public void create() {
+        entity.getEvents().addListener("collisionStart", this::entityCollide);
+        entity.getEvents().addListener("collisionEnd", this::entitySeparate);
         entity.getEvents().addListener("interact", this::attemptInteract);
+
+        super.create();
+    }
+
+    private void entityCollide(Fixture me, Fixture other) {
+        System.out.println("entity collided");
+        Object data = other.getBody().getUserData();
+        if (!(data instanceof BodyUserData userData)) return;
+
+        Entity otherEntity = userData.entity;
+        if (otherEntity.isInteractable()) {
+            collidedEntities.add(otherEntity);
+            System.out.println(collidedEntities);
+        }
+    }
+
+    private void entitySeparate(Fixture me, Fixture other) {
+        Object data = other.getBody().getUserData();
+        if (!(data instanceof BodyUserData userData)) return;
+
+        Entity otherEntity = userData.entity;
+        if (otherEntity.isInteractable()) {
+            collidedEntities.remove(otherEntity);
+        }
     }
 
     private void attemptInteract() {
-        for (Entity e : ServiceLocator.getEntityService().getEntities()) {
-            if (!e.isInteractable()) {
-                continue;
-            }
-
+        Entity closest = null;
+        float closestDistance = Float.MAX_VALUE;
+        for (Entity e : collidedEntities) {
             float distanceFromPlayer = entity.getCenterPosition().dst(e.getCenterPosition());
 
-            float INTERACT_RANGE = 2.0f;
-            if (distanceFromPlayer <= INTERACT_RANGE) {
-                e.getEvents().trigger("interact");
+            if (closest == null) {
+                closest = e;
+                closestDistance = distanceFromPlayer;
+            } else {
+                if (distanceFromPlayer < closestDistance) {
+                    closest = e;
+                    closestDistance = distanceFromPlayer;
+                }
             }
+        }
+
+        if (closest != null) {
+            closest.getEvents().trigger("interact");
         }
     }
 }
