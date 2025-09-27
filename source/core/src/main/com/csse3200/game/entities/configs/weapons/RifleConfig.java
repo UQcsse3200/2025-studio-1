9048195f1f7e58a5bdf1fb2ff86dbc81e16596bc
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-09-27 11:48:11 +1000

    tried setting weapon position while they are created

--- source/core/src/main/com/csse3200/game/entities/configs/weapons/RifleConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/RifleConfig.java
@@ -1,5 +1,7 @@
 package com.csse3200.game.entities.configs.weapons;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class RifleConfig extends RangedWeaponConfig {
     public RifleConfig() {
         ammoCapacity = 50;
