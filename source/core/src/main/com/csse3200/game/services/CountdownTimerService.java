19d3b2345ab1b48d2090f12afcad893f400a70ae
Charmainexoxo <charmainechan@hotmail.com.hk>
2025-09-27 21:45:51 +1000

    Added pause function to countdown timer.

--- source/core/src/main/com/csse3200/game/services/CountdownTimerService.java
+++ source/core/src/main/com/csse3200/game/services/CountdownTimerService.java
@@ -4,21 +4,50 @@ public class CountdownTimerService {
     private final GameTime gameTime;
     private long duration;
     private long startTime;
+    private long pauseTime;
+    private boolean paused;
 
     public CountdownTimerService(GameTime gameTime, long durationMS) {
         this.gameTime = gameTime;
         this.duration = durationMS;
         this.startTime = gameTime.getTime();
+        this.paused = false;
+        this.pauseTime = 0;
     }
 
 
     public long getRemainingMs() {
-        long elapsed = gameTime.getTime() - startTime;
-        long remaining = duration - elapsed;
-        return Math.max(0, remaining);
+        if (isPaused()) {
+            long elapsed = pauseTime - startTime;
+            long remaining = duration - elapsed;
+            return Math.max(0, remaining);
+        } else {
+            long elapsed = gameTime.getTime() - startTime;
+            long remaining = duration - elapsed;
+            return Math.max(0, remaining);
+        }
     }
 
     public boolean isTimeUP() {
         return getRemainingMs() <= 0;
+    }
+
+    public void pause() {
+        if (!isPaused()) {
+            paused = true;
+            pauseTime = gameTime.getTime();
+        }
+    }
+
+    public void resume() {
+        if (paused) {
+            long pausedDuration = gameTime.getTime() - pauseTime;
+            startTime += pausedDuration;
+            paused = false;
+        }
+    }
+
+    public boolean isPaused() {
+        return paused;
     }
 }
