62729f726333b68bdb08378f9736de228fe5da1f
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-09-27 12:01:30 +1000

    getting WeaponConfig to update the name

--- source/core/src/main/com/csse3200/game/entities/configs/weapons/WeaponConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/WeaponConfig.java
@@ -6,4 +6,9 @@ public abstract class WeaponConfig {
     public ItemTypes weaponType = ItemTypes.NONE;
     public int damage = 0;
     public String texturePath = "";
+    private String name = "";
+
+    public void setName(String name) {this.name = name;}
+
+    public String getName() {return name;}
 }
