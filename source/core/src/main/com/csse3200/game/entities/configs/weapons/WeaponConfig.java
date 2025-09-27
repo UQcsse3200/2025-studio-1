372fbc502c7587b67deb60523c6ff6b08815389b
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-09-27 12:23:18 +1000

    Added javadocs for setName() & getName()

--- source/core/src/main/com/csse3200/game/entities/configs/weapons/WeaponConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/weapons/WeaponConfig.java
@@ -8,7 +8,15 @@ public abstract class WeaponConfig {
     public String texturePath = "";
     private String name = "";
 
+    /**
+     * sets the name of the weapon
+     * @param name to be assigned to this weapon configuration
+     */
     public void setName(String name) {this.name = name;}
 
+    /**
+     * returns the name of the weapon
+     * @return the name of this weapon configuration
+     */
     public String getName() {return name;}
 }
