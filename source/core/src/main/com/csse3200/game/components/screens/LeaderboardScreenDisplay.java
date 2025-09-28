32cb965eb6caded6967fbc65a80e82f7db69af09
HitaarnavBhatia <s4898402@student.uq.edu.au>
2025-09-28 19:40:06 +1000

    Added LeaderboardScreen and LeaderboardScreenDisplay for UI and updated GdxGame and PauseMenuDisplay accordingly

---
+++ source/core/src/main/com/csse3200/game/components/screens/LeaderboardScreenDisplay.java
@@ -0,0 +1,19 @@
+package com.csse3200.game.components.screens;
+
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.csse3200.game.GdxGame;
+
+
+
+public class LeaderboardScreenDisplay extends BaseScreenDisplay {
+
+    public LeaderboardScreenDisplay(GdxGame game) {
+        super(game);
+    }
+
+    @Override
+    protected void buildUI(Table root) {
+
+    }
+}
