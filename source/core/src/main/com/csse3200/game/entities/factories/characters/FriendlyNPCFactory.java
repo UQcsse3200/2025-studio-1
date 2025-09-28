f152e2af1d695f2fbd5fc1467ba1f40f11f80efb
Ling Fang <fl3061933275@outlook.com>
2025-09-28 15:03:03 +1000

    basic of the tip fpr friendly npc

--- source/core/src/main/com/csse3200/game/entities/factories/characters/FriendlyNPCFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/characters/FriendlyNPCFactory.java
@@ -1,5 +1,15 @@
 package com.csse3200.game.entities.factories.characters;
 
-public class FriendlyNPCFactory {
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.rendering.TextureRenderComponent;
 
+public class FriendlyNPCFactory {
+    public static Entity createTip() {
+        Entity tip = new Entity()
+                .addComponent(new TextureRenderComponent("images/fireball.png"));
+
+        tip.getComponent(TextureRenderComponent.class).scaleEntity();
+
+        return tip;
+    }
 }
