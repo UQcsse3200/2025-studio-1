4d30e3d7a867cb15f4a7cc9b55117373ce42cab2
xzy-210511 <1612083156@qq.com>
2025-09-28 16:30:39 +1000

    add the picture for tip

--- source/core/src/main/com/csse3200/game/entities/factories/characters/FriendlyNPCFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/characters/FriendlyNPCFactory.java
@@ -6,7 +6,7 @@ import com.csse3200.game.rendering.TextureRenderComponent;
 public class FriendlyNPCFactory {
     public static Entity createTip() {
         Entity tip = new Entity()
-                .addComponent(new TextureRenderComponent("images/fireball.png"));
+                .addComponent(new TextureRenderComponent("images/!.png"));
 
         tip.getComponent(TextureRenderComponent.class).scaleEntity();
 
