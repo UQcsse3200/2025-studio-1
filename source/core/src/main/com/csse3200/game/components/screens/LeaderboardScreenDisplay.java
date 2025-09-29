87796636a1cc4c7fb9c8f02999954921ec56a60f
HitaarnavBhatia <s4898402@student.uq.edu.au>
2025-09-29 13:50:42 +1000

    Improveed UI for LeaderBoards

--- source/core/src/main/com/csse3200/game/components/screens/LeaderboardScreenDisplay.java
+++ source/core/src/main/com/csse3200/game/components/screens/LeaderboardScreenDisplay.java
@@ -2,6 +2,7 @@ package com.csse3200.game.components.screens;
 
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.csse3200.game.GdxGame;
@@ -16,20 +17,23 @@ public class LeaderboardScreenDisplay extends BaseScreenDisplay {
 
     @Override
     protected void buildUI(Table root) {
-
         addTitle(root, "Leaderboard", 2.5f, Color.GOLD, 30f);
 
+        BitmapFont font = new BitmapFont();
+        font.getData().setScale(2f);
+        Label.LabelStyle leaderboardStyle = new Label.LabelStyle(font, Color.CYAN);
+
         Table panel = new Table();
+        panel.setBackground(skin.newDrawable("white", new Color(0, 0, 0, 0.5f)));
         panel.defaults().pad(10f);
 
-        // Temporary sample leaderboard
-        panel.add(new Label("1. Player A - 1200", skin)).row();
-        panel.add(new Label("2. Player B - 1000", skin)).row();
-        panel.add(new Label("3. Player C - 800", skin)).row();
+        // sample leaderboard data
+        panel.add(new Label("1. Player A - 1200", leaderboardStyle)).row();
+        panel.add(new Label("2. Player B - 1000", leaderboardStyle)).row();
+        panel.add(new Label("3. Player C - 800", leaderboardStyle)).row();
 
-        // Back button
         panel.add(button("Back", 1.5f, () -> game.setScreen(GdxGame.ScreenType.MAIN_MENU))).row();
 
-        root.add(panel).center();
+        root.add(panel).center().pad(50f).width(400).height(300);
     }
 }
