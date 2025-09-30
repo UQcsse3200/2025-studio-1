bdc001d92ac08f8f1d80659f9d57f491e9f3507a
Nandini-UQ <n.srivastav@student.uq.edu.au>
2025-09-30 14:09:19 +1000

    added javadocs for RoundData

--- source/core/src/main/com/csse3200/game/records/RoundData.java
+++ source/core/src/main/com/csse3200/game/records/RoundData.java
@@ -1,10 +1,20 @@
 package com.csse3200.game.records;
 
+/**
+ * this class :
+ *      stores the data relevant to calculate score.
+ *      calculates the score.
+ */
 public class RoundData {
     private int currency;
     private float time;  //in seconds
     private int score;   // score = currency + time
 
+    /**
+     *
+     * @param currency earned in that round
+     * @param time taken to complete the round
+     */
     public  RoundData(int currency, float time) {
         this.currency = currency;
         this.time = time;
@@ -20,11 +30,29 @@ public class RoundData {
         return currency + (int)(time);
     }
 
+
+    /**
+     *
+     * @return the currency earned during the round
+     */
     public int getCurrency() {return currency;}
+
+    /**
+     *
+     * @return the time taken to complete the round
+     */
     public float getTime() {return time;}
+
+    /**
+     *
+     * @return the score for the round played
+     */
     public int getScore() {return score;}
 
     @Override
+    /**
+     * to convert & display RoundData objects to string
+     */
     public String toString() {
         return "Currency: " + currency +
             ", Time: " + time + ", " +
