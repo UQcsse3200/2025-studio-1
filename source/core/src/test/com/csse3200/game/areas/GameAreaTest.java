ec682c5b5a9c4edd6eea5211bd01ffd47bacb6a8
Ashton Harms <ashton.harms@student.uq.edu.au>
2025-10-04 01:39:59 +1000

    adding in unit testing

--- source/core/src/test/com/csse3200/game/areas/GameAreaTest.java
+++ source/core/src/test/com/csse3200/game/areas/GameAreaTest.java
@@ -1,5 +1,7 @@
 package com.csse3200.game.areas;
 
+import com.csse3200.game.areas.terrain.TerrainFactory;
+import com.csse3200.game.components.CameraComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.extensions.GameExtension;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.ArrayList;
+import java.lang.reflect.Field;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -36,6 +39,24 @@ class GameAreaTest {
 
         gameArea.dispose();
         verify(mockEntity).dispose();
+    }
 
+    @Test
+    void settingInitialSpawns() throws Exception {
+        ServiceLocator.registerEntityService(new EntityService());
+        TerrainFactory terrainFactory = mock(TerrainFactory.class);
+        CameraComponent cameraComponent = mock(CameraComponent.class);
+
+        Field f1 = ForestGameArea.class.getDeclaredField("PLAYER_SPAWN");
+        Field f2 = ResearchGameArea.class.getDeclaredField("PLAYER_SPAWN");
+        Field f3 = ShippingGameArea.class.getDeclaredField("PLAYER_SPAWN");
+
+        f1.setAccessible(true);
+        f2.setAccessible(true);
+        f3.setAccessible(true);
+
+        assert (f1.get(null) != null);
+        assert (f2.get(null) != null);
+        assert (f3.get(null) != null);
     }
 }
