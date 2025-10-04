047405a64f996166f9af8745ab6c7465678bfa05
ryanhurst14 <ryanhurst1017@gmail.com>
2025-10-04 14:30:33 +1000

    Created touch attack tests for rocket explosion

--- source/core/src/test/com/csse3200/game/components/TouchAttackComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/TouchAttackComponentTest.java
@@ -1,17 +1,33 @@
 package com.csse3200.game.components;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.utils.Array;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.physics.PhysicsLayer;
 import com.csse3200.game.physics.PhysicsService;
+import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.physics.components.PhysicsProjectileComponent;
+import com.csse3200.game.rendering.TextureRenderWithRotationComponent;
+import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(GameExtension.class)
 class TouchAttackComponentTest {
@@ -70,6 +86,90 @@ class TouchAttackComponentTest {
         // This should not cause an exception, but the attack should be ignored
         entity.getEvents().trigger("collisionStart", entityFixture, targetFixture);
     }
+
+    @Nested
+    @DisplayName("Rocket Tests")
+    class rocketTests {
+        ResourceService resourceService;
+        EntityService entityService;
+        @BeforeEach
+        void setup() {
+            resourceService = mock(ResourceService.class);
+            entityService = mock(EntityService.class);
+            ServiceLocator.registerEntityService(entityService);
+            //Mock animation and textures
+            Texture texture = mock(Texture.class);
+            TextureAtlas textureAtlas = mock(TextureAtlas.class);
+            Array<TextureAtlas.AtlasRegion> regions = new Array<>();
+            regions.add(mock(TextureAtlas.AtlasRegion.class));
+            when(textureAtlas.findRegions(anyString())).thenReturn(regions);
+            when(resourceService.getAsset(anyString(), eq(Texture.class))).thenReturn(texture);
+            when(resourceService.getAsset(anyString(), eq(TextureAtlas.class))).thenReturn(textureAtlas);
+            ServiceLocator.registerResourceService(resourceService);
+        }
+
+        @Test
+        void rocketLauncherBulletsExplodeOnImpact() {
+            //Make the bullet
+            short targetLayer = (1 << 3);
+            Entity entity = createBullet(targetLayer, true);
+            // Target does not have a combat component
+            Entity target =
+                    new Entity()
+                            .addComponent(new PhysicsComponent())
+                            .addComponent(new HitboxComponent().setLayer(targetLayer));
+            target.create();
+
+            Fixture entityFixture = entity.getComponent(HitboxComponent.class).getFixture();
+            Fixture targetFixture = target.getComponent(HitboxComponent.class).getFixture();
+
+            // This should not cause an exception, but the attack should be ignored
+            entity.getEvents().trigger("collisionStart", entityFixture, targetFixture);
+            //Verify the atlas was got
+            verify(resourceService).getAsset("images/rocketExplosion.atlas", TextureAtlas.class);
+            verify(entityService).register(any(Entity.class)); //Registered explosion
+        }
+
+        @Test
+        void bulletsDontExplodeOnImpact() {
+            //Make the bullet
+            short targetLayer = (1 << 3);
+            Entity entity = createBullet(targetLayer, false);
+            // Target does not have a combat component
+            Entity target =
+                    new Entity()
+                            .addComponent(new PhysicsComponent())
+                            .addComponent(new HitboxComponent().setLayer(targetLayer));
+            target.create();
+
+            Fixture entityFixture = entity.getComponent(HitboxComponent.class).getFixture();
+            Fixture targetFixture = target.getComponent(HitboxComponent.class).getFixture();
+
+            // This should not cause an exception, but the attack should be ignored
+            entity.getEvents().trigger("collisionStart", entityFixture, targetFixture);
+            //Verify the atlas was never got
+            verify(resourceService, never()).getAsset("images/rocketExplosion.atlas", TextureAtlas.class);
+            verify(entityService, never()).register(any(Entity.class));
+        }
+    }
+
+    Entity createBullet(short targetLayer, boolean rocket) {
+
+        WeaponsStatsComponent newWeaponStats = new WeaponsStatsComponent(10);
+        PhysicsProjectileComponent ppc = mock(PhysicsProjectileComponent.class);
+        when(ppc.getPrio()).thenReturn(ComponentPriority.MEDIUM);
+        newWeaponStats.setRocket(rocket);
+        Entity entity = new Entity()
+                .addComponent(new PhysicsComponent())
+                .addComponent(ppc)
+                .addComponent(newWeaponStats)
+                .addComponent(new HitboxComponent())
+                .addComponent(new TouchAttackComponent(targetLayer));
+        entity.create();
+        return entity;
+    }
+
+
 
 
 
