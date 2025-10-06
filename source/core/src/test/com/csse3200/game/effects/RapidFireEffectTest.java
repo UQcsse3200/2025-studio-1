20525f0f7136d815125a0b156725b2b47d43f064
Cailin Delgado <c.delgado@uqconnect.edu.au>
2025-10-06 21:45:40 +1000

    Added tests for the RapidFireEffect file

--- source/core/src/test/com/csse3200/game/effects/RapidFireEffectTest.java
+++ source/core/src/test/com/csse3200/game/effects/RapidFireEffectTest.java
@@ -1,4 +1,59 @@
 package com.csse3200.game.effects;
 
+import com.csse3200.game.components.WeaponsStatsComponent;
+import com.csse3200.game.entities.Entity;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
 public class RapidFireEffectTest {
+    RapidFireEffect rapidFireEffect;
+    WeaponsStatsComponent weaponsStatsComponent;
+
+    @BeforeEach
+    void setUp() {
+        rapidFireEffect = new RapidFireEffect(10);
+        weaponsStatsComponent = mock(WeaponsStatsComponent.class);
+        when(weaponsStatsComponent.getCoolDown()).thenReturn(20f);
+    }
+
+    @Test
+    void shouldApplyEffect() {
+        Entity weapon = new Entity().addComponent(weaponsStatsComponent);
+        assertTrue(rapidFireEffect.apply(weapon));
+        assertTrue(rapidFireEffect.isActive());
+    }
+
+    @Test
+    @Description("null WeaponStatsComponent")
+    void shouldNotApplyEffect() {
+        assertFalse(rapidFireEffect.apply(new Entity()));
+        assertFalse(rapidFireEffect.isActive());
+    }
+
+    @Test
+    void shouldNotFinishDuration() {
+        Entity weapon = mock(Entity.class);
+        when(weapon.getComponent(WeaponsStatsComponent.class)).thenReturn(weaponsStatsComponent);
+        rapidFireEffect.apply(weapon);
+
+        rapidFireEffect.update(1f);
+        assertTrue(rapidFireEffect.isActive());
+        verify(weaponsStatsComponent, never()).setCoolDown(20f);
+    }
+
+    @Test
+    void shouldFinishDuration() {
+        Entity weapon = mock(Entity.class);
+        when(weapon.getComponent(WeaponsStatsComponent.class)).thenReturn(weaponsStatsComponent);
+        rapidFireEffect.apply(weapon);
+
+        rapidFireEffect.update(10f);
+        assertFalse(rapidFireEffect.isActive());
+        verify(weaponsStatsComponent, times(1)).setCoolDown(20f);
+    }
 }
