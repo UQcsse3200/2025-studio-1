8e708a6f18fc016601e0c54d419385e879e68f33
Feiy-Shen <feiyang.shen@student.uq.edu.au>
2025-10-06 10:32:49 +1000

    added Red and Blue Variance to some enemies

---
+++ source/core/src/main/com/csse3200/game/entities/configs/characters/VroombaBlueConfig.java
@@ -0,0 +1,8 @@
+package com.csse3200.game.entities.configs.characters;
+
+/**
+ * Defines the properties stored in ghost king config files to be loaded by the NPC Factory.
+ */
+public class VroombaBlueConfig extends BaseEntityConfig {
+    public int cleaningFactor = 1;
+}
