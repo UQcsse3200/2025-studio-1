b74b30ad151c0cad60d6bfd3e7129dbcff9fe69a
Jason Zhong <jasonyzhong06@gmail.com>
2025-10-07 23:11:33 +1000

    feat: cue rotation

--- source/core/src/main/com/csse3200/game/components/minigames/pool/PoolGame.java
+++ source/core/src/main/com/csse3200/game/components/minigames/pool/PoolGame.java
@@ -1,7 +1,6 @@
 package com.csse3200.game.components.minigames.pool;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.RayCastCallback;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Timer;
