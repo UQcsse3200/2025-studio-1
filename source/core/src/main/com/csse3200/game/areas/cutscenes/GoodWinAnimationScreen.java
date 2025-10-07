ea68e32ad1a906d292e0a5ea24d01c32b8b94e77
jholmesuq <jonny.holmes@student.uq.edu.au>
2025-10-08 07:24:10 +1000

    Created BadWinAnimation and made it the parent class to GoodWinAnimation

--- source/core/src/main/com/csse3200/game/areas/cutscenes/GoodWinAnimationScreen.java
+++ source/core/src/main/com/csse3200/game/areas/cutscenes/GoodWinAnimationScreen.java
@@ -1,30 +1,16 @@
 package com.csse3200.game.areas.cutscenes;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.GridPoint2;
-import com.csse3200.game.areas.GameArea;
-import com.csse3200.game.areas.MainHall;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.components.CameraComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.rendering.AnimationRenderComponent;
-import com.csse3200.game.rendering.TextureRenderComponent;
-import com.csse3200.game.services.ServiceLocator;
 
-public class GoodWinAnimationScreen extends GameArea {
+
+public class GoodWinAnimationScreen extends BadWinAnimationScreen {
     private static final GridPoint2 PLAYER_COORDS = new GridPoint2(-10,15);
-    private static final GridPoint2[] EXPLOSION_COORDS = new GridPoint2[]{
-            new GridPoint2(77,69),
-            new GridPoint2(54,69),
-            new GridPoint2(57,49),
-            new GridPoint2(52,24),
-            new GridPoint2(74,45),
-            new GridPoint2(78, 24),
-            new GridPoint2(50, 45)
-    };
-
-    final static float EXPLOSION_FRAME_DURATION = 0.2f;
+    private static final float PLAYER_FRAME_DURATION = 0.15f;
 
 
     public GoodWinAnimationScreen(TerrainFactory terrainFactory, CameraComponent cameraComponent) {
@@ -32,26 +18,13 @@ public class GoodWinAnimationScreen extends GameArea {
     }
 
     public void create() {
-        ensureAssets();
-        terrain = terrainFactory.createTerrain(TerrainFactory.TerrainType.WIN_SCREEN);
-        spawnEntity(new Entity().addComponent(terrain));
-
+        super.create();
         spawnPlayer();
-        spawnExplosions();
     }
 
     @Override
     public String toString() {
         return "GoodWinAnimation";
-    }
-
-    /**
-     * allows manipulation of player character by loading function
-     *
-     * @return player entity
-     */
-    public Entity getPlayer() {
-        return null; //Handles cleanly without errors
     }
 
     private void ensureAssets() {
@@ -70,16 +43,7 @@ public class GoodWinAnimationScreen extends GameArea {
         return createAnimation(
                 "images/player.atlas",
                 "left_run",
-                0.15f,
-                Animation.PlayMode.LOOP,
-                1f);
-    }
-
-    private AnimationRenderComponent explosionAnimation() {
-        return createAnimation(
-                "images/explosion_2.atlas",
-                "explosion_2",
-                EXPLOSION_FRAME_DURATION,
+                PLAYER_FRAME_DURATION,
                 Animation.PlayMode.LOOP,
                 1f);
     }
@@ -92,34 +56,7 @@ public class GoodWinAnimationScreen extends GameArea {
         spawnEntity(player);
     }
 
-    private void spawnExplosions() {
-        for (GridPoint2 explosionCoord : EXPLOSION_COORDS) {
-            Entity explosion = new Entity()
-                        .addComponent(explosionAnimation());
-            explosion.setPosition(x(explosionCoord.x), y(explosionCoord.y));
-            spawnEntity(explosion);
-        }
-    }
-
-    private AnimationRenderComponent createAnimation(String atlasPath, String animationName, float frameDuration, Animation.PlayMode playMode, float width) {
-        AnimationRenderComponent animator = new AnimationRenderComponent(
-                ServiceLocator.getResourceService()
-                        .getAsset(atlasPath, TextureAtlas.class));
-        animator.addAnimation(animationName, frameDuration, playMode);
-        //animator.scaleEntity(width);
-        animator.startAnimation(animationName);
-        return animator;
-    }
-
     public static GoodWinAnimationScreen load(TerrainFactory terrainFactory, CameraComponent camera) {
         return (new GoodWinAnimationScreen(terrainFactory, camera));
-    }
-
-    private float x(int value) {
-        return value * 14 / 100f;
-    }
-
-    private float y(int value) {
-        return value * 8 / 100f + 3;
     }
 }
