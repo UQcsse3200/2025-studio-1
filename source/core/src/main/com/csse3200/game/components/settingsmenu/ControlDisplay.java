64263ea5e59d7c77c563089bc42b659925e91983
adityarajsingh <adityaraj.singh@student.uq.edu.au>
2025-10-16 23:19:27 +1000

    added a control button for keybindings tutorial

---
+++ source/core/src/main/com/csse3200/game/components/settingsmenu/ControlDisplay.java
@@ -0,0 +1,16 @@
+package com.csse3200.game.components.settingsmenu;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class ControlDisplay {
+    public ControlDisplay(SettingsMenuDisplay settingsMenuDisplay, Stage stage) {
+    }
+
+    public void create() {
+    }
+
+    public Actor getRootTable() {
+        return null;
+    }
+}
