88489fb94e5e9b5b6da5a11077cade6f0579453e
Ashton Harms <ashton.harms@student.uq.edu.au>
2025-10-20 13:57:06 +1000

    adds roomstatetest

---
+++ source/core/src/test/com/csse3200/game/areas/RoomStateTest.java
@@ -0,0 +1,39 @@
+package com.csse3200.game.areas;
+
+import com.badlogic.gdx.math.GridPoint2;
+import com.csse3200.game.areas.terrain.TerrainFactory;
+import com.csse3200.game.components.CameraComponent;
+import com.csse3200.game.components.DoorComponent;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.EntityService;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(GameExtension.class)
+class RoomStateTest {
+
+    private TerrainFactory terrainFactory;
+    private CameraComponent cameraComponent;
+
+    @BeforeEach
+    void beforeEach() {
+        ServiceLocator.registerEntityService(new EntityService());
+        terrainFactory = mock(TerrainFactory.class);
+        cameraComponent = mock(CameraComponent.class);
+    }
+
+    @Test
+    void testLockDoors() {
+        GameArea reception = new Reception(terrainFactory, cameraComponent);
+        Entity door = new Entity().addComponent(new DoorComponent(() -> {}));
+        reception.registerDoors(new Entity[]{door});
+        assert(door.getComponent(DoorComponent.class).isLocked());
+    }
+}
