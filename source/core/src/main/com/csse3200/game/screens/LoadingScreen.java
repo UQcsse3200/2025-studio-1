b99ed162c55a432679e63e2de23dc7c2ffafb697
HunterYCSU <hunter.yicong.su@gmail.com>
2025-10-22 03:48:55 +1000

    Update LoadingScreen.java

--- source/core/src/main/com/csse3200/game/screens/LoadingScreen.java
+++ source/core/src/main/com/csse3200/game/screens/LoadingScreen.java
@@ -9,10 +9,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.csse3200.game.GdxGame;
 
-
 /**
  * Loading screen with background and large text.
- * Ensures the screen is actually drawn before switching to MAIN_GAME.
  */
 public class LoadingScreen extends ScreenAdapter {
     private final GdxGame game;
@@ -24,29 +22,24 @@ public class LoadingScreen extends ScreenAdapter {
 
     public LoadingScreen(GdxGame game) {
         this.game = game;
-
         this.batch = new SpriteBatch();
         this.background = new Texture("images/menu_background.png");
-
-        // Use default bitmap font and scale it up
         this.font = new BitmapFont();
-        font.getData().setScale(3f); // Make text bigger
+        font.getData().setScale(3f);
     }
 
     @Override
     public void render(float delta) {
-        // Clear screen
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Draw background and text
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         font.draw(batch, "LOADING...", Gdx.graphics.getWidth() / 2f - 100,
                 Gdx.graphics.getHeight() / 2f);
         batch.end();
 
-        // Switch to main game safely after first render
+        // Switch to main game after first render
         if (firstFrame) {
             firstFrame = false;
             Gdx.app.postRunnable(() -> game.setScreen(GdxGame.ScreenType.MAIN_GAME));
