f0ec7082f4ef99d5d3d98c2a9e7ce7874b9f1e0b
rileyrushton05 <rileyrushton05@gmail.com>
2025-10-21 11:09:35 +1000

    Removed LightingDefaults for simplicity

--- source/core/src/main/com/csse3200/game/lighting/LightingEngine.java
+++ source/core/src/main/com/csse3200/game/lighting/LightingEngine.java
@@ -5,25 +5,43 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Disposable;
 import com.csse3200.game.components.CameraComponent;
 
+/**
+ * Processes lighting components using the Box2DLight library. Sets up the ray handler that is responsible
+ * for rendering all lights to the screen.
+ */
 public class LightingEngine implements Disposable {
+    private static final float AMBIENT_LIGHT = 0.75f;
+
     private final RayHandler rayHandler;
     private CameraComponent camera;
 
-    private float ambientLight = LightingDefaults.AMBIENT_LIGHT;
+    private float ambientLight = AMBIENT_LIGHT;
 
+    /**
+     * Constructor method for the lighting engine. This is where some of the rayHandler's
+     * global variables are set and can be changed.
+     *
+     * @param camera The camera associated with the current renderer for the screen.
+     * @param world The same world registered with the physics engine.
+     */
     public LightingEngine(CameraComponent camera, World world) {
         this.camera = camera;
         this.rayHandler = new RayHandler(world);
 
         RayHandler.setGammaCorrection(true);
         RayHandler.useDiffuseLight(false);
-        rayHandler.setAmbientLight(0.75f);
+        rayHandler.setAmbientLight(AMBIENT_LIGHT);
 
         rayHandler.setBlur(true);
         rayHandler.setBlurNum(1);
     }
 
-    // Injectable/testing ctor
+    /**
+     * Injectable constructor method.
+     *
+     * @param rayHandler mock rayHandler
+     * @param camera camera component
+     */
     LightingEngine(RayHandler handler, CameraComponent camera) {
         this.rayHandler = handler;
         this.camera = camera;
